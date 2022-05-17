52588b3e890acbb984ccee98d8df63d9de21332a
kotaitos <saikota0314@gmail.com>
2022-05-17 23:03:36 +0900

    :recycle:app bar

--- lib/ui/screens/accounts_screen.dart
+++ lib/ui/screens/accounts_screen.dart
@@ -1,7 +1,6 @@
 import 'package:allergy/l10n/l10n.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import '../widget/tabbed_app_bar.dart';
 
 // [Themelist] インスタンスにおける処理。
 class Accounts extends StatelessWidget {
