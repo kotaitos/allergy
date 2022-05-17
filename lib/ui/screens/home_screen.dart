2be4c7d63f3aed17cf7b06229ee8d4984f7aa0cd
kotaitos <saikota0314@gmail.com>
2022-05-18 00:38:53 +0900

    :+1:home design

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -20,10 +20,53 @@ class HomeScreen extends StatelessWidget {
         child: Column(mainAxisSize: MainAxisSize.min, children: [
           Padding(
             padding: const EdgeInsets.fromLTRB(0, 0, 0, 20.0),
-            child: Text(l10n.ui__home__title,
+            child: Text(l10n.ui__home__your_qr,
                 style:
                     const TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
           ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 0, 0, 20.0),
+            child: QrImage(
+                data: 'https://www.kamo-it.org/blog/36/',
+                version: QrVersions.auto,
+                size: 200.0),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(30.0, 0, 30.0, 0),
+            child: ElevatedButton(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Icon(Icons.qr_code_scanner),
+                  Text('QRコードをスキャンする'),
+                ],
+              ),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.orange,
+                onPrimary: Colors.black,
+                shape: const StadiumBorder(),
+              ),
+              onPressed: () {},
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(30.0, 0, 30.0, 0),
+            child: ElevatedButton(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Icon(Icons.push_pin),
+                  Text('アレルギーを登録する'),
+                ],
+              ),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.lightBlue,
+                onPrimary: Colors.black,
+                shape: const StadiumBorder(),
+              ),
+              onPressed: () {},
+            ),
+          )
         ]),
       ),
     );
